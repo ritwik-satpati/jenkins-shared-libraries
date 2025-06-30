3a08afe795d93ae221d24174e85d665d127cc214
Ritwik Satpati <74023275+ritwik-satpati@users.noreply.github.com>
2025-07-01 00:44:15 +0530

    Update DockerHub_Image_Push.groovy

--- vars/DockerHub_Image_Push.groovy
+++ vars/DockerHub_Image_Push.groovy
@@ -1,6 +1,6 @@
 def call(String DockerHubCred, String ProjectName, String ImageTag ){
   echo "--- Pushing the Image to DockerHub : Start ---"
-  withCredentials([usernamePassword(credentialsId:${DockerHubCred}, usernameVariable:"dockerHubUser", passwordVariable:"dockerHubPass")]){
+  withCredentials([usernamePassword(credentialsId:"${DockerHubCred}", usernameVariable:"dockerHubUser", passwordVariable:"dockerHubPass")]){
     sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
     // sh "docker image tag ${ProjectName}:${ImageTag} ${env.dockerHubUser}/${ProjectName}:${ImageTag}"
     sh "docker push ${env.dockerHubUser}/${ProjectName}:${ImageTag}"
